194a626eb59d27136a1527f22a4e4b83791dc3fe
MANCM026 <Mohit.X.Manchanda.-ND@disney.com>
2018-04-27 12:19:51 +0530

    personal info

---
+++ src/main/java/Info/personal/App.java
@@ -0,0 +1,13 @@
+package Info.personal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
